ac974f7104f599bb602d42986526afcfb2f29090
viktorstrate <viktorstrate@gmail.com>
2014-11-10 20:10:11 +0100

    Added pistion rightclick event

--- src/main/java/dk/qpqp/randomrewards/Main.java
+++ src/main/java/dk/qpqp/randomrewards/Main.java
@@ -1,7 +1,19 @@
 package dk.qpqp.randomrewards;
 
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
-
+	Plugin plugin = this;
+	PluginDescriptionFile pdf = plugin.getDescription();
+	
+	public void onEnable() {
+        getServer().getPluginManager().registerEvents(new Listener(), this);
+        getLogger().info(pdf.getName()+" v. "+pdf.getVersion()+" has been enabled!");
+    }
+	
+	public void onDisable(){
+		getLogger().info(pdf.getName()+" v. "+pdf.getVersion()+" has been disabled!");
+	}
 }
