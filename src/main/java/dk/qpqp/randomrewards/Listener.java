179bf4320c1432a3776143342ccc8539855aafe4
viktorstrate <viktorstrate@gmail.com>
2014-11-11 16:30:02 +0100

    Started on the changes to the new config, dot done yet!

--- src/main/java/dk/qpqp/randomrewards/Listener.java
+++ src/main/java/dk/qpqp/randomrewards/Listener.java
@@ -9,28 +9,27 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.material.MaterialData;
 import org.bukkit.plugin.Plugin;
 
 public class Listener implements org.bukkit.event.Listener {
 	
+	Main main;
 	Plugin plugin;
 	
-	public Listener(Plugin pl) {
-		plugin = pl;
+	public Listener(Main main) {
+		this.main = main;
+		plugin = main.plugin;
 	}
 	
 	@SuppressWarnings({ "deprecation", "unchecked" })
 	@EventHandler
 	public void onInteract(PlayerInteractEvent event){
 		if(event.getAction() == Action.RIGHT_CLICK_BLOCK){
+			
 			Block block = event.getClickedBlock();
 			if(block.getType()==Material.WOOL&&block.getData()==7){
-				List<Integer> list = (List<Integer>) plugin.getConfig().getList("Rewards.Items");
-				int randomItem = (int)(Math.random()*list.size());
-				ItemStack item = new ItemStack(list.get(randomItem), 1);
-				event.getPlayer().getInventory().addItem(item);
-				event.getPlayer().updateInventory();
-				event.getPlayer().sendMessage("You got a "+item.getType().name());
+				Material randomMaterial = main.randomItems.get(Math.random()*main.randomItems.size());
 			}
 		}
 	}
--- src/main/java/dk/qpqp/randomrewards/Main.java
+++ src/main/java/dk/qpqp/randomrewards/Main.java
@@ -4,7 +4,10 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Set;
 
+import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
@@ -18,18 +21,52 @@ public final class Main extends JavaPlugin {
 	File configFile;
 	FileConfiguration config;
 	
+	public HashMap<Integer, Material> randomItems;
+	public HashMap<Integer, Integer> randomItemsAmount;
+	public HashMap<Integer, Byte> randomItemsData;
+	
 	public void onEnable() {
-        getServer().getPluginManager().registerEvents(new Listener(plugin), this);
+        getServer().getPluginManager().registerEvents(new Listener(this), this);
         getLogger().info(pdf.getName()+" v. "+pdf.getVersion()+" has been enabled!");
         
         configFile = new File(getDataFolder(), "config.yml");
         config = new YamlConfiguration();
         
+        // Creates the config if it doesn't exists
         try{
         	firstRun();
         } catch (Exception e){
         	e.printStackTrace();
         }
+        
+        // Find the random items from the config
+        randomItems = new HashMap<Integer, Material>();
+        randomItemsAmount = new HashMap<Integer, Integer>();
+        randomItemsData = new HashMap<Integer, Byte>();
+        Set<String> keys = plugin.getConfig().getKeys(true);
+		for(String str: keys){
+			Message.log(str, plugin);
+			
+			if(str.contains("rewards.items.")){
+				Message.log("YO", plugin);
+				
+				for(Material mat: Material.values()){
+					
+					if(str.matches("rewards.items."+mat.name())){
+						Message.log("MEGA YO "+mat.name(), plugin);
+						randomItems.put(randomItems.size(), mat);
+					}
+					
+					for(String strData: keys){
+						if(strData.matches("rewards.items."+mat.name()+".data")){
+							Message.log("MEGA YO DATA "+strData, plugin);
+						}
+					}
+					
+					
+				}
+			}
+		}
     }
 	
 	public void onDisable(){
