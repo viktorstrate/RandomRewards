008af0148bcbb73967cb974208ab7ba429cb5351
viktorstrate <viktorstrate@gmail.com>
2014-11-15 14:24:33 +0100

    The new config system is now working

--- src/main/java/dk/qpqp/randomrewards/ConfigSetup.java
+++ src/main/java/dk/qpqp/randomrewards/ConfigSetup.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
@@ -13,184 +12,209 @@ import java.util.regex.Pattern;
 
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 
 public class ConfigSetup {
-	
+
 	// This file creates and loads the config.yml file
-	
+
 	private File configFile;
 	private Plugin plugin;
-	
+
 	private HashMap<Integer, ConfigItem> rewards, prices;
 	private ConfigItem rewardBlock;
-	
-	public ConfigSetup(Plugin plugin){
+
+	public ConfigSetup(Plugin plugin) {
 		this.plugin = plugin;
 		configFile = new File(plugin.getDataFolder(), "config.yml");
-        
-        // Creates the config if it doesn't exists
-        try{
-        	firstRun();
-        } catch (Exception e){
-        	e.printStackTrace();
-        }
-        
-        rewards = loadItems("rewards");
-        prices = loadItems("price.items");
-        rewardBlock = loadItem("rewardBlock");
-        
-	}
-	
-	private void firstRun() throws Exception {
-		if(!configFile.exists()){
-			Message.log("Config not found creating one...", plugin);
-	        configFile.getParentFile().mkdirs();
-	        copy(plugin.getResource("config.yml"), configFile);
-	    }
-	}
-	// Is used to, copy the config.yml from this plugin.jar to the folder plugins/RandomRewards/
-	private void copy(InputStream in, File file){
-		try {
-	        OutputStream out = new FileOutputStream(file);
-	        byte[] buf = new byte[1024];
-	        int len;
-	        while((len=in.read(buf))>0){
-	            out.write(buf,0,len);
-	        }
-	        out.close();
-	        in.close();
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    }
-	}
-	
-	public HashMap<Integer, ConfigItem> loadItems(String pathToItems){
 
-		return new HashMap<Integer, ConfigItem>();
+		// Creates the config if it doesn't exists
+		try {
+			firstRun();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		List<String> rewardList = plugin.getConfig().getStringList("rewards");
+		List<String> priceList = plugin.getConfig().getStringList("price");
+		rewards = loadItems(rewardList);
+		prices = loadItems(priceList);
+		rewardBlock = new ConfigItem(Material.WOOL, 1, (short) 3);
+		
+		Message.log("REWARDS"+rewards.get(0).enchantmentsLevel.get(0), plugin);
+
 	}
-	
-	public ConfigItem loadItem(String pathToItem){
-		// Loads items from the config.yml
-		Material item = Material.STICK;
-		int itemAmount = -1;
-		Short itemData = 0;
-		ArrayList<ConfigEnchantment> enchantments = new ArrayList<ConfigEnchantment>();
-        Set<String> keys = plugin.getConfig().getKeys(true);
-		for(String str: keys){
+
+	private void firstRun() throws Exception {
+		if (!configFile.exists()) {
+			Message.log("Config not found creating one...", plugin);
+			configFile.getParentFile().mkdirs();
+			copy(plugin.getResource("config.yml"), configFile);
+		}
+	}
+
+	// Is used to, copy the config.yml from this plugin.jar to the folder
+	// plugins/RandomRewards/
+	private void copy(InputStream in, File file) {
+		try {
+			OutputStream out = new FileOutputStream(file);
+			byte[] buf = new byte[1024];
+			int len;
+			while ((len = in.read(buf)) > 0) {
+				out.write(buf, 0, len);
+			}
+			out.close();
+			in.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public HashMap<Integer, ConfigItem> loadItems(List<String> values) {
+
+		HashMap<Integer, ConfigItem> items = new HashMap<Integer, ConfigItem>();
+		Message.log("Items loaded from the config.yml", plugin);
+		for (String value : values) {
 			
-			if(str.startsWith(pathToItem+".")){
-				
-				for(Material mat: Material.values()){
-					
-					if( str.endsWith(mat.name()) ){
-						item = mat;
-						
-						// Loads the datavalue of the item(s)
-						if(plugin.getConfig().get(pathToItem+"."+mat.name()+".data")!=null){
-							itemData = (short) plugin.getConfig().getInt(pathToItem+"."+mat.name()+".data");
-						}
-						
-						// Loads the amount of items
-						if(plugin.getConfig().get(pathToItem+"."+mat.name()+".amount")!=null){
-							itemAmount = plugin.getConfig().getInt(pathToItem+"."+mat.name()+".amount");
-						}
-						
-						// Loads the enchantments
-						enchantments = loadEnchantments(pathToItem+"."+mat.name());
-						
-					}
-					
-				}
-				break;
-			}
+			items.put(items.size(), loadItem(value));
+
 		}
-		
-		
-		
-		if(itemAmount<=0){
-			Message.log("Item "+pathToItem+" loaded", plugin);
-			return new ConfigItem(item, itemAmount, itemData, enchantments);
-		} else {
-			Message.warning("Item "+pathToItem+" didn't load correctly, check the config", plugin);
-			return null;
-		}
-		
+
+		return items;
+
 	}
-	
-	// Loads enchantments from the config, function used by function loadItems() and loadItem()
-	public ArrayList<ConfigEnchantment> loadEnchantments(String pathToEnchantments){
-		ArrayList<ConfigEnchantment> list = new ArrayList<ConfigEnchantment>();
-		
-		// A key with all the nodes in the config.yml
-		Set<String> encKeys = plugin.getConfig().getKeys(true);
-		
-		// Loops through the nodes in the config.yml
-		for(String str: encKeys){
-			// If found the path to the enchantments
-			if(str.startsWith(pathToEnchantments+".")){
-				// Loops through all the avaliable enchantment types
-				for(Enchantment enc: Enchantment.values()){
-					// If found one matching the one(s) in the config.yml
-					if(str.endsWith(enc.getName())){
-						// Sends message and adds it to the list
-						Message.log("With enchantment: "+enc.getName(), plugin);
-						int level = 1;
-						level = plugin.getConfig().getInt(pathToEnchantments+"."+enc.getName()+".level");
-						list.add(new ConfigEnchantment(enc, level));
+
+	@SuppressWarnings("deprecation")
+	public ConfigItem loadItem(String value) {
+		String regex = "(\\d)+";
+		Matcher regexMatcher = regexChecker(regex, value);
+
+		int id = 0;
+		int amount = 1;
+		Short datavalue = 0;
+
+		HashMap<Integer, Enchantment> EnchantmentTypes = new HashMap<Integer, Enchantment>();
+		HashMap<Integer, Integer> EncnantmentLevel = new HashMap<Integer, Integer>();
+
+		int idEnd = 0;
+		int amountEnd = 0;
+		while (regexMatcher.find()) {
+			if (regexMatcher.group().length() != 0) {
+				if (regexMatcher.start() == 0) {
+
+					try {
+						id = Integer.parseInt(regexMatcher.group().trim());
+						Message.log("Item: " + Material.getMaterial(id), plugin);
+					} catch (NumberFormatException e) {
+						System.out.println("The id is not a number");
+					}
+
+					idEnd = regexMatcher.end();
+				}
+
+				if (regexMatcher.start() == idEnd + 1) {
+
+					try {
+						amount = Integer.parseInt(regexMatcher.group()
+								.trim());
+						Message.log("- Amount: "+amount, plugin);
+					} catch (NumberFormatException e) {
+						System.out.println("Amount not found");
+					}
+
+					amountEnd = regexMatcher.end();
+				}
+
+				if (regexMatcher.start() == amountEnd + 1) {
+
+					try {
+						datavalue = (short) Integer.parseInt(regexMatcher.group().trim());
+					} catch (NumberFormatException e) {
+						System.out.println("Datavalue not found");
 					}
 				}
 			}
 		}
-		return list;
+		
+		Set<String> encKey = ConfigItem.getEnchantmentNames().keySet();
+		String enchantmentsStr = "";
+		for(String str: encKey){
+			enchantmentsStr+=str+"|";
+		}
+		// removes the last character from the string
+		if (enchantmentsStr.length() > 0 && enchantmentsStr.charAt(enchantmentsStr.length()-1)=='|') {
+			enchantmentsStr = enchantmentsStr.substring(0, enchantmentsStr.length()-1);
+		}
+		
+		String regexEnc = "("+enchantmentsStr+")\\s\\d+";
+		Matcher regexMatcherEnc = regexChecker(regexEnc, value);
+		
+		while (regexMatcherEnc.find()) {
+			// Find the type
+			String regexEncType = "[a-zA-Z]+";
+			Matcher regexMatcherEncType = regexChecker(regexEncType,
+					regexMatcherEnc.group().trim());
+
+			regexMatcherEncType.find();
+			EnchantmentTypes.put(EnchantmentTypes.size(), ConfigItem.getEnchantmentNames().get(regexMatcherEncType.group().trim()));
+			Message.log("DEBUG "+EnchantmentTypes.get(0).getName(), plugin);
+			
+
+			// Find the level
+			String regexEncLvl = "\\d+";
+			Matcher regexMatcherEncLvl = regexChecker(regexEncLvl,
+					regexMatcherEnc.group().trim());
+
+			regexMatcherEncLvl.find();
+			EncnantmentLevel.put(EnchantmentTypes.size() - 1,
+					Integer.parseInt(regexMatcherEncLvl.group().trim()));
+			Message.log("- - "+ConfigItem.getEnchantmentNames().get(regexMatcherEncType.group().trim()).getName()+" with "+regexMatcherEncLvl.group().trim()+" levels", plugin);
+		}
+
+		return new ConfigItem(Material.getMaterial(id), amount, datavalue, EnchantmentTypes, EncnantmentLevel);
 	}
-	
-	public ConfigItem loadBlock(String pathToBlock){
+
+	private Matcher regexChecker(String theRegx, String str2Check) {
+		Pattern checkRegex = Pattern.compile(theRegx);
+		Matcher regexMatcher = checkRegex.matcher(str2Check);
+
+		return regexMatcher;
+	}
+
+	public ConfigItem loadBlock(String pathToBlock) {
 		// Loads items from the config.yml
 		Material item = Material.STONE;
 		Short itemData = -1;
-        Set<String> keys = plugin.getConfig().getKeys(true);
-		for(String str: keys){
-			
-			if(str.startsWith(pathToBlock+".")){
-				
-				for(Material mat: Material.values()){
-					
-					if( str.endsWith(mat.name()) ){
+		Set<String> keys = plugin.getConfig().getKeys(true);
+		for (String str : keys) {
+
+			if (str.startsWith(pathToBlock + ".")) {
+
+				for (Material mat : Material.values()) {
+
+					if (str.endsWith(mat.name())) {
 						item = mat;
-						
-						if(plugin.getConfig().get(pathToBlock+"."+mat.name()+".data")!=null){
-							itemData = (short) plugin.getConfig().getInt(pathToBlock+"."+mat.name()+".data");
+
+						if (plugin.getConfig().get(
+								pathToBlock + "." + mat.name() + ".data") != null) {
+							itemData = (short) plugin.getConfig().getInt(
+									pathToBlock + "." + mat.name() + ".data");
 						}
 					}
-					
+
 				}
 				break;
 			}
 		}
-		if(itemData>=0){
-			Message.log("Block "+pathToBlock+" loaded", plugin);
+		if (itemData >= 0) {
+			Message.log("Block " + pathToBlock + " loaded", plugin);
 			return new ConfigItem(item, 1, itemData);
 		} else {
-			Message.warning("Block "+pathToBlock+" didn't load correctly, check the config", plugin);
+			Message.warning("Block " + pathToBlock
+					+ " didn't load correctly, check the config", plugin);
 			return null;
 		}
-		
-		
-	}
-	
-	public ArrayList<String> regxChecker(String theRegx, String str2Check){
-		ArrayList<String> list = new ArrayList<String>();
-		
-		List<ItemStack> lis = new ArrayList<ItemStack>();
-		lis.add(new ItemStack(Material.DIAMOND));
-		lis.add(new ItemStack(Material.STICK));
-		lis.add(new ItemStack(Material.GOLD_AXE));
-		plugin.getConfig().set("itm", lis);
-		plugin.saveConfig();
-		
-		return list;
+
 	}
 
 	public HashMap<Integer, ConfigItem> getRewards() {
@@ -200,8 +224,8 @@ public class ConfigSetup {
 	public HashMap<Integer, ConfigItem> getPrices() {
 		return prices;
 	}
-	
-	public ConfigItem getRewardBlock(){
+
+	public ConfigItem getRewardBlock() {
 		return rewardBlock;
 	}
 }
